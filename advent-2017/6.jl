311deb2f9b092f4bd758edaa07bcdf795c8de31d
Kevin James <KevinJames@thekev.in>
2017-12-05 21:26:57 -0800

    feat(advent): day 6

---
+++ advent-2017/6.jl
@@ -0,0 +1,45 @@
+# https://adventofcode.com/2017/day/6
+function reallocate_once(data::Array)
+    m = maximum(data)
+    idx = find(x -> x == m, data)[1]
+
+    value = 0
+    for (n, i) in enumerate(Iterators.cycle(1:length(data)))
+        if n < idx
+            continue
+        end
+
+        if n == idx
+            value = data[i]
+            data[i] = 0
+            continue
+        end
+
+        data[i] += 1
+        value -= 1
+        if value == 0
+            return data
+        end
+    end
+end
+
+function reallocate(data::Array)
+    states = []
+    for i in Iterators.countfrom(1)
+        push!(states, data)
+        data = reallocate_once(copy(data))
+        if data in states
+            return i, data
+        end
+    end
+end
+
+# https://adventofcode.com/2017/day/6/input
+data = [0, 5, 10, 0, 11, 14, 13, 4, 11, 8, 8, 7, 1, 4, 12, 11]
+
+idx, state = reallocate(copy(data))
+println(idx)
+
+idx, state = reallocate(copy(data))
+idx, state = reallocate(copy(state))
+println(idx)
