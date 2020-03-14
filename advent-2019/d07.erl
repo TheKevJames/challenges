d0b43133cc772873d44975ae0c02a61da59b7e17
Kevin James <KevinJames@thekev.in>
2020-03-13 19:42:05 -0700

    feat(advent): solve 2019.7b (message passing!)

--- advent-2019/d07.erl
+++ advent-2019/d07.erl
@@ -1,5 +1,5 @@
 -module(d07).
--export([main/0]).
+-export([main/0, exec/2]).
 
 readlines(File) ->
     {ok, Data} = file:read_file(File),
@@ -18,61 +18,75 @@ param(I, X, L) ->
         0 -> nth(nth(I+X, L), L)
     end.
 
-exec(L, Inputs) -> exec(L, 0, Inputs, []).
-exec(L, I, Inputs, Output) ->
+exec(L, Output) -> exec(L, 0, Output).
+exec(L, I, Output) ->
     case nth(I, L) rem 100 of
         1 ->
             LVal = param(I, 1, L),
             RVal = param(I, 2, L),
             Val = LVal + RVal,
-            exec(replace(L, nth(I+3, L), Val), I+4, Inputs, Output);
+            exec(replace(L, nth(I+3, L), Val), I+4, Output);
         2 ->
             LVal = param(I, 1, L),
             RVal = param(I, 2, L),
             Val = LVal * RVal,
-            exec(replace(L, nth(I+3, L), Val), I+4, Inputs, Output);
+            exec(replace(L, nth(I+3, L), Val), I+4, Output);
         3 ->
-            Val = lists:nth(1, Inputs),
-            exec(replace(L, nth(I+1, L), Val), I+2, lists:nthtail(1, Inputs), Output);
+            receive Val -> ok end,
+            exec(replace(L, nth(I+1, L), Val), I+2, Output);
         4 ->
             Val = param(I, 1, L),
-            exec(L, I+2, Inputs, Output ++ [Val]);
+            Output ! Val,
+            exec(L, I+2, Output);
         5 ->
             LVal = param(I, 1, L),
             RVal = param(I, 2, L),
             IP = if LVal /= 0 -> RVal; true -> I+3 end,
-            exec(L, IP, Inputs, Output);
+            exec(L, IP, Output);
         6 ->
             LVal = param(I, 1, L),
             RVal = param(I, 2, L),
             IP = if LVal == 0 -> RVal; true -> I+3 end,
-            exec(L, IP, Inputs, Output);
+            exec(L, IP, Output);
         7 ->
             LVal = param(I, 1, L),
             RVal = param(I, 2, L),
             Val = if LVal < RVal -> 1; true -> 0 end,
-            exec(replace(L, nth(I+3, L), Val), I+4, Inputs, Output);
+            exec(replace(L, nth(I+3, L), Val), I+4, Output);
         8 ->
             LVal = param(I, 1, L),
             RVal = param(I, 2, L),
             Val = if LVal == RVal -> 1; true -> 0 end,
-            exec(replace(L, nth(I+3, L), Val), I+4, Inputs, Output);
-        99 -> Output;
+            exec(replace(L, nth(I+3, L), Val), I+4, Output);
+        99 -> 0;
         _Else -> error(io_lib:format("invalid opcode: ~w", [nth(I,L)]))
     end.
 
-execChain(L, Inputs) -> execChain(L, Inputs, 0).
-execChain(L, Inputs, Start) -> lists:foldl(fun(Input, Acc) -> exec(L, [Input] ++ Acc) end, [Start], Inputs).
+loop(PID) ->
+    receive
+        Data ->
+            case is_process_alive(PID) of
+                true -> PID ! Data, loop(PID);
+                false -> Data
+            end
+    end.
+run(L, [In0, In1, In2, In3, In4]) ->
+    PID4 = spawn(d07, exec, [L, self()]),
+    PID4 ! In4,
+    PID3 = spawn(d07, exec, [L, PID4]),
+    PID3 ! In3,
+    PID2 = spawn(d07, exec, [L, PID3]),
+    PID2 ! In2,
+    PID1 = spawn(d07, exec, [L, PID2]),
+    PID1 ! In1,
+    PID0 = spawn(d07, exec, [L, PID1]),
+    PID0 ! In0,
 
-execLoop(L, Inputs) -> execLoop(L, Inputs, 0).
-execLoop(L, Inputs, Start) ->
-    [Val] = execChain(L, Inputs, Start),
-    execLoop(L, Inputs, Val).
+    self() ! 0,
+    loop(PID0).
 
-part1(L) -> lists:max(lists:flatten([execChain(L, Ins) || Ins <- perms(lists:seq(0, 4))])).
-% TODO: part2 requires the `exec` loops receive Inputs of arbitrary lengths and do not shut down between iterations
-% ...rewrite to use messaging passing?
-part2(L) -> [execLoop(L, Ins) || Ins <- perms(lists:seq(5, 9))].
+part1(L) -> lists:max([run(L, Ins) || Ins <- perms(lists:seq(0, 4))]).
+part2(L) -> lists:max([run(L, Ins) || Ins <- perms(lists:seq(5, 9))]).
 
 main() ->
     L = readlines("in07"),
