41df77631fe77af5320e17822b7efffb2a2fc0ee
Kevin James <KevinJames@thekev.in>
2020-12-02 17:54:07 -0800

    feat(advent): solve 2020.01

---
+++ advent-2020/README.rst
@@ -0,0 +1,8 @@
+Advent of Code 2020
+===================
+
+These are my solutions to 2020's `Advent of Code`_, written in `Nim`_ since I
+didn't get around to properly finishing in 2018!
+
+.. _Advent of Code: http://adventofcode.com/2020
+.. _Nim: https://nim-lang.org/
---
+++ advent-2020/d01.nim
@@ -0,0 +1,17 @@
+import sequtils
+import strutils
+
+proc part1(xs: seq[int], target = 2020): int =
+  for x in xs:
+    if target-x in xs:
+      return x * (target-x)
+
+proc part2(xs: seq[int]): int =
+  for i, x in xs:
+    var res = part1(xs[i .. high(xs)], target=2020-x)
+    if res != 0:
+      return x * res
+
+let f = map(strip(readFile("in01")).splitLines(), parseInt)
+echo f.part1()
+echo f.part2()
