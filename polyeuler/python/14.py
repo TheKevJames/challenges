8e6223ba585feb8e5435101dd3c18b05563e0d49
Kevin James <KevinJames@thekev.in>
2025-05-21 16:18:29 +0100

    style(polyeuler): fix lint

--- polyeuler/python/14.py
+++ polyeuler/python/14.py
@@ -9,4 +9,4 @@ def collatz(n):
     return collatz(3 * n + 1 if n % 2 else n // 2) + 1
 
 
-print(sorted({collatz(n): n for n in range(1, 1_000_000)}.items())[-1][1])
+print(max({collatz(n): n for n in range(1, 1_000_000)}.items())[1])
